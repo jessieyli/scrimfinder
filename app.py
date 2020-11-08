747cfa1e31c05c5030abc9d525ccc7a94a7fa8bb
jessieyli <50643746+jessieyli@users.noreply.github.com>
2020-11-07 20:15:35 -0800

    html form

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from flask import request, redirect
 import os
 import datetime
 
 app = Flask(__name__)
 
 app.secret_key = os.urandom(24)
+
+@app.route('/submitteam', methods = ['POST'])
+def signup():
+    name = request.form['teamname']
+    print("The registered team is" + name + "'")
+    return redirect('/')
 
 @app.route('/')
 def index():
